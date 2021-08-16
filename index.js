d95854db5e7fc3001d7bab74d4e9885c62ea68f8
Njorogefaith132 <wairimunjoroge132@gmail.com>
2021-08-16 09:51:38 +0300

    Additions

---
+++ index.js
@@ -0,0 +1,30 @@
+const btnResume = document.querySelector(".resume-button");
+
+btnResume.addEventListener("mouseover", changeColor);
+btnResume.addEventListener("mouseout", changeColors);
+
+
+function changeColor() {
+  btnResume.style.backgroundColor = "white";
+  btnResume.style.color = "black";
+
+  btnResume.innerHTML("Click me");
+  btnResume.style.transform = "translateX()";
+}
+
+function changeColors() {
+  btnResume.style.backgroundColor = "black";
+  btnResume.style.color = "#EDC441";
+  btnResume.innerHTML("Click me");
+  btnResume.style.transform = "translateX()";
+}
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
