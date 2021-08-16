3e6336e2f5b6c98ba612d4b7c8cd15b1c8022f72
Njorogefaith132 <wairimunjoroge132@gmail.com>
2021-08-16 10:27:25 +0300

    Sidebar changes

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ function changeColors() {
 
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+   
   }
   
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+    
   }
