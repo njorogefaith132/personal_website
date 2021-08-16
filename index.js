01d866807c3ee395140f36fac28461d0d14d1212
Njorogefaith132 <wairimunjoroge132@gmail.com>
2021-08-16 17:36:47 +0300

    Footer and contact form

--- index.js
+++ index.js
@@ -3,27 +3,45 @@ const btnResume = document.querySelector(".resume-button");
 btnResume.addEventListener("mouseover", changeColor);
 btnResume.addEventListener("mouseout", changeColors);
 
-
 function changeColor() {
   btnResume.style.backgroundColor = "white";
   btnResume.style.color = "black";
   btnResume.innerHTML("Click me");
-  
 }
 
 function changeColors() {
   btnResume.style.backgroundColor = "black";
   btnResume.style.color = "#EDC441";
   btnResume.innerHTML("Click me");
-  
 }
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-   
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    
-  }
+  document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
+const form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const status = document.getElementById("status");
+  const data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      status.innerHTML = "Thanks for your submission!";
+      status.classList.add("success");
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+      status.classList.add("error");
+    });
+}
+form.addEventListener("submit", handleSubmit);
